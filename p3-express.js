f82fc61764bd0fd45e1112f2ec4eaab264f7d235
alu0100812534 <alu0100812534@ull.edu.es>
2017-03-06 23:10:04 +0000

    update

--- p3-express.js
+++ p3-express.js
@@ -1,9 +1,7 @@
-var express = require('express');
+var express = require('express')
 var app = express()
 var path = require('path');
 
-
-app.set('port', (process.env.PORT || 5000));
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -11,10 +9,16 @@ app.set('view engine', 'ejs');
 // Host the book.
 app.use(express.static(path.join(__dirname, '/')));
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
 /*
  var router = express.Router();
   module.exports = router;
 */
+
+var server = app.listen(81, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log('Escuchando en http://%s:%s', host, port)
+
+})
