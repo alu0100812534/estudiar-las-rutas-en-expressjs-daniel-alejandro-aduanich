ad65c3e293962ff32c36305d3e0a0f9285e0cf44
alu0100845808 <alu0100845808@ull.edu.es>
2017-03-07 00:59:31 +0000

    Basic routing ejemplo

---
+++ src/Br.js
@@ -0,0 +1,11 @@
+
+const express = require('express')
+const app = express()
+var port = 3000
+app.get('/', function (req, res) {
+  res.send('This is our home page!')
+})
+
+app.listen(port, function () {
+  console.log('We are listening on port ' + port)
+})
